439974263a2e7b8a190c1faf5778d29d5ec8259a
Inbal Prital <inbalprital@gmail.com>
2022-06-22 19:11:05 +0300

    our second commit

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,20 +1,33 @@
-import { storageService } from './storage.service.js';
+import { storageService } from './storage.service.js'
+
+const API_KEY = 'AIzaSyB_chQszcvUfSeiALf9bvdQw44SIPjbHq4'
 
 export const locService = {
-    getLocs
+  getLocs,
 }
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+  { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+  { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
 ]
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs);
-        }, 2000)
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs)
+    }, 2000)
+  })
 }
 
+getCordsByCity()
 
+function getCordsByCity(city = 'tokyo') {
+  console.log('hey')
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${city}+View,+CA&key=${API_KEY}`
+    )
+    .then(({ data }) => {
+      console.log(data)
+    })
+}
