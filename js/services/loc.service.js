b7ed5b4fb6b819cc22b358f10627c13a140b0584
Inbal Prital <inbalprital@gmail.com>
2022-06-22 23:31:17 +0300

    4th commit

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -3,39 +3,40 @@ import { storageService } from './storage.service.js'
 const API_KEY = 'AIzaSyB_chQszcvUfSeiALf9bvdQw44SIPjbHq4'
 
 export const locService = {
-    getLocs,
+  getLocs,
 }
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+  { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+  { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
 ]
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs)
-        }, 2000)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs)
+    }, 2000)
+  })
+}
+
+getCordsByCity('tel aviv')
+
+function getCordsByCity(city) {
+  console.log('hey')
+  axios
+    .get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${city}+View,+CA&key=${API_KEY}`
+    )
+
+    .then(({ data }) => data.results)
+    .then((results) => {
+      let resultsPrms = results.map(loadCityData)
+      return Promise.all(resultsPrms).then((results) => {
+        return results
+      })
     })
 }
 
-getCordsByCity()
-
-function getCordsByCity(city) {
-    console.log('hey')
-    axios
-        .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}+View,+CA&key=${API_KEY}`)
-
-        .then(({ data }) => data.results)
-        .then((results) => {
-            let resultsPrms = results.map(loadCityData)
-            return Promise.all(resultsPrms)
-                .then(results => {
-                    return results
-                })
-        })
-}
-
 function loadCityData(city) {
-    console.log(city);
+  console.log(city)
 }
