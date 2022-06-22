eb294ea2d24f3b6c84109381dd01f23919a023ea
stav kotlovsky <stavkot1999@gmail.com>
2022-06-22 19:46:32 +0300

    small changes

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -3,31 +3,39 @@ import { storageService } from './storage.service.js'
 const API_KEY = 'AIzaSyB_chQszcvUfSeiALf9bvdQw44SIPjbHq4'
 
 export const locService = {
-  getLocs,
+    getLocs,
 }
 
 const locs = [
-  { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
-  { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
 ]
 
 function getLocs() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(locs)
-    }, 2000)
-  })
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(locs)
+        }, 2000)
+    })
 }
 
 getCordsByCity()
 
-function getCordsByCity(city = 'tokyo') {
-  console.log('hey')
-  axios
-    .get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${city}+View,+CA&key=${API_KEY}`
-    )
-    .then(({ data }) => {
-      console.log(data)
-    })
+function getCordsByCity(city) {
+    console.log('hey')
+    axios
+        .get(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}+View,+CA&key=${API_KEY}`)
+
+        .then(({ data }) => data.results)
+        .then((results) => {
+            let resultsPrms = results.map(loadCityData)
+            return Promise.all(resultsPrms)
+                .then(results => {
+                    return results
+                })
+        })
+}
+
+function loadCityData(city) {
+    console.log(city);
 }
